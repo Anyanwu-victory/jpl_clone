531d80b14ed74b0e929e6b9e4eb8f3ea365d7d0d
Anyanwu-victory <118054739+Anyanwu-victory@users.noreply.github.com>
2024-01-27 08:03:51 +0100

    done 27th January

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    
+    listStyleType: {
+      
+    },
+    
+
+    screens: {
+      'sm': '375px',
+
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
+    extend: {
+      
+      colors:{
+        Tomato: 'hsl(4, 100%, 67%)',
+        DarkSlateGrey: 'hsl(234, 29%, 20%)',
+        CharcoalGrey: 'hsl(235, 18%, 26%)',
+        Grey: 'hsl(231, 7%, 60%)',
+        White: 'hsl(0, 0%, 100%)',
+
+      },
+      backgroundImage : {
+        'jpl_Logo': "url('/img/jpl_logo-removebg-preview.png')",
+
+  
+      },
+    },
+  },
+  plugins: [],
+}
+
